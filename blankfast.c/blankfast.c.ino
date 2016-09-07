cafdc8de2f6995e9a1deef1cfcf3af923999d924
Nathan Baker <pcnate@gmail.com>
2016-09-07 00:47:58 -0500

    added serial detect, added power status updates over serial

--- blankfast.c/blankfast.c.ino
+++ blankfast.c/blankfast.c.ino
@@ -1,38 +1,69 @@
 // Simple LED blink
 
-const int BUTTON_DEBOUNCE_DELAY = 50;
+const int BUTTON_DEBOUNCE_DELAY = 2500;
 const int LED_CLOCK_DELAY = 49;
+
+const bool POWER_OFF = HIGH;
 
 const int led = LED_BUILTIN;
 
-const int led1 = 15;
-const int led2 = 14;
-const int led3 = 12;
+const int outputPower = 15;
+const int outputData = 14;
+const int outputSerial = 12;
+
+const int powerDetect = 7;
+const int userButton1 = 8;
 
 unsigned long buttonClock = 0;
 unsigned long ledClock = 0;
+
+bool serialStatus = false;
 bool ledState = false;
+bool powerState = false;
 
 void setup() {
 
   // buttons
-  pinMode( 7, INPUT_PULLUP );
-  pinMode( 8, INPUT_PULLUP );
+  pinMode( powerDetect, INPUT_PULLUP );
+  pinMode( userButton1, INPUT_PULLUP );
 
   // led outputs
-  pinMode( led1, OUTPUT );
-  pinMode( led2, OUTPUT );
-  pinMode( led3, OUTPUT );
+  pinMode( outputPower,  OUTPUT );
+  pinMode( outputData,   OUTPUT );
+  pinMode( outputSerial, OUTPUT );
 
   // built in LED
-  pinMode(led, OUTPUT);
+  pinMode( led, OUTPUT );
 
   Serial.begin( 9600 );
 
 }
 
+void powerOn() {
+
+}
+
+void dataOn() {
+
+}
+
+void powerOff() {
+
+}
+
 void loop() {
 
+  if( !serialStatus && Serial.dtr() ) {
+    serialStatus = true;
+    digitalWrite( outputSerial, serialStatus );
+    Serial.println( powerState ? "POWER_ON" : "POWER_OFF");
+  }
+  if( serialStatus && !Serial.dtr() ) {
+    serialStatus = false;
+    digitalWrite( outputSerial, serialStatus );
+  }
+
+  // flash the led
   if( ( millis() - ledClock ) >= LED_CLOCK_DELAY ) {
     digitalWrite(led, ledState ? HIGH : LOW );
     ledState = !ledState;
@@ -41,22 +72,27 @@ void loop() {
 
   // check for power
   if( ( millis() - buttonClock ) >= BUTTON_DEBOUNCE_DELAY ) {
-    if( digitalRead(7) == LOW ) {
-      Serial.println("Button1 is pressed");
-      digitalWrite( led1, LOW );
+    if( digitalRead( powerDetect ) == POWER_OFF ) {
+      digitalWrite( outputPower, LOW );
+      if( powerState ) {
+        powerState = false;
+        Serial.println("POWER_OFF");
+      }
     } else {
-      digitalWrite( led1, HIGH );
+      digitalWrite( outputPower, HIGH );
+      if( !powerState ) {
+        powerState = true;
+        Serial.println("POWER_ON");
+      }
     }
-    if( digitalRead(8) == LOW ) {
+    if( digitalRead( userButton1 ) == LOW ) {
       Serial.println("Button2 is pressed");
-      digitalWrite( led2, LOW );
-      digitalWrite( led3, HIGH );
+      digitalWrite( outputData, HIGH );
     } else {
-      digitalWrite( led2, HIGH );
-      digitalWrite( led3, LOW );
+      digitalWrite( outputData, LOW );
     }
 
     buttonClock = millis();
   }
-  
+
 }
